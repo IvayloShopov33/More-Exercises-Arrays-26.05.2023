d0783acf84be40475e39a295a7dc75bc289f950c
Ивайло Шопов <is41062491@edu.mon.bg>
2023-05-30 20:10:49 +0300

    Add project files.

---
+++ 1. Encrypt, Sort, and Print Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1._Encrypt__Sort__and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stringsCount = int.Parse(Console.ReadLine());
+            int[] sumOfStrings = new int[stringsCount];
+            for (int i = 0; i < stringsCount; i++)
+            {
+                string strings = Console.ReadLine();
+                char[] arrayOfChars = strings.ToCharArray();
+                int sum = 0;
+                for (int j = 0; j < arrayOfChars.Length; j++)
+                {
+                    if (arrayOfChars[j] == 'a' || arrayOfChars[j] == 'e' || arrayOfChars[j] == 'i' || arrayOfChars[j] == 'o' || arrayOfChars[j] =='u' ||
+                        arrayOfChars[j] == 'A' || arrayOfChars[j] == 'E' || arrayOfChars[j] == 'I' || arrayOfChars[j] == 'O' || arrayOfChars[j] == 'U')
+                    {
+                        int vowels = arrayOfChars[j] * arrayOfChars.Length;
+                        sum += vowels;
+                    }
+                    else
+                    {
+                        int consonants = arrayOfChars[j] / arrayOfChars.Length;
+                        sum += consonants;
+                    }                    
+                }
+                sumOfStrings[i] = sum;
+            }
+            Array.Sort(sumOfStrings);
+            for (int i = 0; i < sumOfStrings.Length; i++)
+            {
+                Console.WriteLine(sumOfStrings[i]);
+            }
+        }
+    }
+}
---
+++ 2. Pascal Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _2._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                int n = int.Parse(Console.ReadLine());
+
+                long[][] pascalTriangle = new long[n][];
+
+                for (int i = 0; i < n; i++)
+                {
+                    pascalTriangle[i] = new long[i + 1];
+                    pascalTriangle[i][0] = 1;
+
+                    for (int j = 1; j < i; j++)
+                    {
+                        pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+                    }
+
+                    pascalTriangle[i][i] = 1;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write(pascalTriangle[i][j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+
+
